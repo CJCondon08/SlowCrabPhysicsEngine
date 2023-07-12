a99c6339a5e196a490a6c56acca527a8b0baeb9e
MashBrown <cjcondon0@gmail.com>
2023-07-11 20:03:05 -0500

    Newton's first two laws with drag system

--- src/main.rs
+++ src/main.rs
@@ -12,38 +12,38 @@ fn main() {
     let height:usize = 700;
 
     let background_color: u32 = from_u8_rgb(70, 70, 70);
-    let mut window = Window::new("Slow Crab", width, height, WindowOptions::default()).unwrap();
+    let mut window: Window = Window::new("Slow Crab", width, height, WindowOptions::default()).unwrap();
     
     let mut object_list: Vec<object::Object> = Vec::new();
-    let square_size = 150;
-
+    let square_size: usize = 150;
     let mut delta_timer:f32 = 0.01;
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        let loop_time = Instant::now();
+        let loop_time: Instant = Instant::now();
 
+        
         for i in 0..object_list.len() {
             object_list[i].gravity(delta_timer);
-            object_list[i].drag(&mut window);
+            object_list[i].is_colliding(object_list.as_ref(), i);
+            object_list[i].drag(&mut window, delta_timer);
 
         }
 
         if window.is_key_pressed(Key::S, minifb::KeyRepeat::No) {
             let mut square = object::Object::new_rigid();
-            square.y = 0;
-            square.x = 0 + ((square_size+10) * object_list.len());
+            //square.y = 0;
+            square.x = 20 + ((square_size+10) * object_list.len()) as i16;
             object_list.push(square);
         }
 
-        // Clear the window
+        // Clear the windowobject_list[i].
         let buffer = &mut vec![background_color; width * (height + 10)];
         
         //Draw objects
-        //remove floor and make pos an i16 or i32
         for z in 0..object_list.len(){
-            for y in object_list[z].y ..(object_list[z].y + square_size) {
-                for x in object_list[z].x ..(object_list[z].x + square_size) {
-                    let index = x + (width * y) as usize;
+            for y in object_list[z].y ..(object_list[z].y + square_size as i16) {
+                for x in object_list[z].x ..(object_list[z].x + square_size as i16) {
+                    let index: usize = x as usize + (width * y as usize);
                     buffer[index] = from_u8_rgb(40, 80, 200);
                 }
             }
@@ -55,6 +55,6 @@ fn main() {
         delta_timer = loop_time.elapsed().as_secs_f32();
         drop(loop_time);
 
-        println!("{}", 1.0/delta_timer);
+        //println!("{}", 1.0/delta_timer);
     }
 }
--- src/object.rs
+++ src/object.rs
@@ -1,45 +1,80 @@
 use minifb::Window;
 
 pub struct Object {
-    pub x: usize,
-    pub y: usize,
-    pub size: usize,
-    pub mass: f32,
-    pub acceleration: f32,
-    pub rigid: bool,
-    pub fall_time: f32
+    pub x: i16,
+    pub y: i16,
+    prev: (i16, i16),
+    size: i16,
+    mass: f32,
+    acceleration: (f32, f32),
+    rigid: bool
 }
 
 impl Object {
     pub fn new_rigid() -> Object {
-        Object{x: 0, y: 0, size: 150, mass: 10.0, acceleration: 0.0, rigid: true, fall_time: 0.0}
+        Object{x: 0, y: 200, prev: (0, 200), size: 150, mass: 5.0, acceleration: (0.0, 0.0), rigid: true}
     }
 
     pub fn new_non_rigid() -> Object {
-        Object{x: 0, y: 0, size: 150, mass: 0.0, acceleration: 0.0, rigid: false, fall_time: 0.0}
+        Object{x: 0, y: 0, prev: (0, 0), size: 150, mass: 0.0, acceleration: (0.0, 0.0), rigid: false}
     }
 
-    pub fn gravity(&mut self, delta_timer: f32) -> usize{
-
+    pub fn gravity(&mut self, delta_timer: f32) -> i16{
         //change to collision detection
         if self.y >= 540 || !self.rigid{
-            self.acceleration = 0.0;
-            self.fall_time = 0.0;
+            self.acceleration.1 = 0.0;
+            self.acceleration.0 = 0.0;
             return self.y;
         }
-        self.fall_time += delta_timer;
 
-        let g: f32 = 9.8*3.0;
-        self.acceleration = g*self.fall_time;
-        self.y += self.acceleration as usize;
-            
+        let g: f32 = -9.8*3.0;
+        self.acceleration.1 -= g*delta_timer;  
+        self.y += self.acceleration.1.floor() as i16;
+        
+        let air_resistance:f32 = 5.0;
+        self.acceleration.0 -= air_resistance*delta_timer;
+        self.x -= self.acceleration.0 as i16;
         self.y
     }
 
-    pub fn drag(&mut self, window: &mut Window) -> (usize, usize){
+    fn motion(&mut self, delta_timer: f32, greater_pos:i16, lesser_pos:i16) -> f32 {
+        let mut force: f32;
 
-        let mouse_x = window.get_mouse_pos(minifb::MouseMode::Clamp).unwrap().0 as usize;
-        let mouse_y = window.get_mouse_pos(minifb::MouseMode::Clamp).unwrap().1 as usize;
+        force = (greater_pos - lesser_pos) as f32;
+        force *= delta_timer*130.0;
+        self.acceleration.1 = force/self.mass;
+        self.acceleration.1
+    }
+
+    pub fn collision_effects(/*&mut self, object2: &mut Object*/){
+        //self.acceleration.0 += object2.acceleration.0*object2.mass;
+        //self.acceleration.1 += object2.acceleration.1*object2.mass;
+        //object2.acceleration.0 += self.acceleration.0*self.mass;
+        //object2.acceleration.0 += self.acceleration.0*self.mass;
+    }
+
+    pub fn is_colliding(&mut self, objects_list: &Vec<Object>, index: usize) -> bool{
+        let mut i = index;
+
+        if objects_list.len() < 2 {
+            return false;
+        }
+
+        while i < objects_list.len() -1 {
+            if self.x < objects_list[i].x+objects_list[i].size{
+                if self.x+self.size > objects_list[i].x {
+                    Object::collision_effects();
+                }
+            }
+            i += 1;
+        }
+        return true;
+    }
+
+    pub fn drag(&mut self, window: &mut Window, delta_timer: f32) -> (i16, i16){
+
+        let mouse_x: i16 = window.get_mouse_pos(minifb::MouseMode::Clamp).unwrap().0 as i16;
+        let mouse_y: i16 = window.get_mouse_pos(minifb::MouseMode::Clamp).unwrap().1 as i16;
 
         if !window.get_mouse_down(minifb::MouseButton::Left) 
         || mouse_y <= self.y || mouse_y >= self.y + self.size 
@@ -49,23 +84,27 @@ impl Object {
 
         drop(mouse_x);
         drop(mouse_y);
+        self.prev.0 = self.x;
+        self.prev.1 = self.y;
 
         let mut is_rigid:bool = false;
 
         if self.rigid {
             self.rigid = false;
             is_rigid = true;
-            self.acceleration = 0.0;
-            self.fall_time = 0.0;
+            self.acceleration.1 = 0.0;
+            self.acceleration.0 = 0.0;
         }
 
-        self.x = window.get_mouse_pos(minifb::MouseMode::Clamp).unwrap().0 as usize - self.size/2;
-        self.y = window.get_mouse_pos(minifb::MouseMode::Clamp).unwrap().1 as usize - self.size/2;
+        self.x = window.get_mouse_pos(minifb::MouseMode::Clamp).unwrap().0 as i16 - self.size/2;
+        self.y = window.get_mouse_pos(minifb::MouseMode::Clamp).unwrap().1 as i16 - self.size/2;
 
         if is_rigid {
             self.rigid = true;
         }
 
+        self.acceleration.0 = self.motion(delta_timer, self.prev.0, self.x);
+        self.acceleration.1 = self.motion(delta_timer, self.y, self.prev.1);
         return (self.x, self.y);
     }
 }
